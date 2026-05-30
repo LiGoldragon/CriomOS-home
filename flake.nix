cc6bb3d2b35fbbd745a7f588893932a8dbe468a9
li <li@goldragon.criome.net>
2026-05-30 12:39:41 +0200

    home: update production spirit

--- flake.nix
+++ flake.nix
@@ -139,7 +139,7 @@
     persona-spirit-v0-1-1.inputs.nixpkgs.follows = "nixpkgs";
     persona-spirit-v0-2-0.url = "github:LiGoldragon/persona-spirit?ref=v0.2.0";
     persona-spirit-v0-2-0.inputs.nixpkgs.follows = "nixpkgs";
-    persona-spirit-v0-3-0.url = "github:LiGoldragon/persona-spirit?ref=v0.3.0";
+    persona-spirit-v0-3-0.url = "github:LiGoldragon/persona-spirit?ref=v0.3.0.1";
     persona-spirit-v0-3-0.inputs.nixpkgs.follows = "nixpkgs";
     persona-spirit-next.url = "github:LiGoldragon/persona-spirit?ref=main";
     persona-spirit-next.inputs.nixpkgs.follows = "nixpkgs";
