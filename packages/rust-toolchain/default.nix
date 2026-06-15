ce31cc21bfa54646e34504e27d818f6d59fc3de9
li <li@goldragon.criome.net>
2026-06-15 07:41:42 +0200

    pin home rust toolchain version

--- packages/rust-toolchain/default.nix
+++ packages/rust-toolchain/default.nix
@@ -1,6 +1,6 @@
 { inputs, pkgs, ... }:
 
-(inputs.rust-overlay.lib.mkRustBin { } pkgs).stable.latest.minimal.override {
+(inputs.rust-overlay.lib.mkRustBin { } pkgs).stable."1.96.0".minimal.override {
   extensions = [
     "rust-src"
     "rust-analyzer"
