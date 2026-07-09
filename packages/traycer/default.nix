669aa2d17c3fb53936a329ee65de7ef7df947e18
li <li@goldragon.criome.net>
2026-07-09 12:33:40 +0200

    home: run Traycer host CLI under FHS
    Agent: operating-system-implementer pi sub-agent.

--- packages/traycer/default.nix
+++ packages/traycer/default.nix
@@ -11,12 +11,51 @@ let
     hash = "sha256-8vi/E3MYKh5vDRG9vh7AKKNfNI7VZHlqbqJO6jXP5Yc=";
   };
 
-  appimageContents = pkgs.appimageTools.extractType2 {
+  rawAppimageContents = pkgs.appimageTools.extractType2 {
     inherit pname version src;
   };
+
+  traycerCliFhs = pkgs.buildFHSEnv {
+    pname = "traycer-cli-linux-compat";
+    inherit version;
+
+    targetPkgs = pkgs: pkgs.appimageTools.defaultFhsEnvArgs.targetPkgs pkgs;
+    multiPkgs = pkgs: pkgs.appimageTools.defaultFhsEnvArgs.multiPkgs pkgs;
+    runScript = "${rawAppimageContents}/resources/cli/linux-x64/traycer";
+  };
+
+  appimageContents = pkgs.runCommand "${pname}-${version}-nix-extracted" { } ''
+        cp -a ${rawAppimageContents} "$out"
+        chmod -R u+w "$out"
+
+        cliDir="$out/resources/cli/linux-x64"
+        test -x "$cliDir/traycer"
+        mv "$cliDir/traycer" "$cliDir/traycer-unwrapped"
+    printf '%s\n' \
+      '#!${pkgs.runtimeShell}' \
+      'exec ${traycerCliFhs}/bin/traycer-cli-linux-compat "$@"' \
+      > "$cliDir/traycer"
+    chmod 755 "$cliDir/traycer"
+  '';
 in
-pkgs.appimageTools.wrapType2 {
-  inherit pname version src;
+pkgs.appimageTools.wrapAppImage {
+  inherit pname version;
+  src = appimageContents;
+
+  extraPreBwrapCmds = ''
+    traycer_cli_manifest="''${HOME:-}/.traycer/cli/manifest.json"
+    traycer_cli_link="''${HOME:-}/.traycer/cli/bin/traycer"
+    traycer_cli_target="${appimageContents}/resources/cli/linux-x64/traycer"
+    if [ -n "''${HOME:-}" ] \
+      && [ -f "$traycer_cli_manifest" ] \
+      && [ -L "$traycer_cli_link" ] \
+      && grep -q '"source"[[:space:]]*:[[:space:]]*"desktop"' "$traycer_cli_manifest"; then
+      current_target="$(readlink "$traycer_cli_link" || true)"
+      if [ "$current_target" != "$traycer_cli_target" ]; then
+        ln -sfn "$traycer_cli_target" "$traycer_cli_link"
+      fi
+    fi
+  '';
 
   extraInstallCommands = ''
     install -Dm444 ${appimageContents}/traycer-desktop.desktop \
