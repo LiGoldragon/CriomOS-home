2888bc0933aa7578d2e4ecfdf8857eef7c0377dd
li <li@goldragon.criome.net>
2026-07-19 13:29:16 +0200

    home: provide GnuPG to orchestrate daemon

--- modules/home/profiles/min/orchestrate.nix
+++ modules/home/profiles/min/orchestrate.nix
@@ -71,7 +71,7 @@ in
       Service = {
         RuntimeDirectory = "orchestrate";
         RuntimeDirectoryMode = "0700";
-        Environment = [ "PATH=${lib.makeBinPath [ pkgs.jujutsu ]}" ];
+        Environment = [ "PATH=${lib.makeBinPath [ pkgs.gnupg pkgs.jujutsu ]}" ];
         ExecStartPre = "${orchestratePackage}/bin/orchestrate-write-configuration ${signalPath} ${storePath} ${ordinarySocketPath} ${metaSocketPath} ${upgradeSocketPath} ${workspaceRoot} ${gitIndexRoot} messenger=${messengerSocketPath}";
         ExecStart = "${orchestratePackage}/bin/orchestrate-daemon ${signalPath}";
         Restart = "on-failure";
