b692b52a5d8a8b3a0628099edf2c9432157a910d
li <li@goldragon.criome.net>
2026-09-05 02:10:55 +0200

    Harden Wispr status freshness checks

--- checks/wispr-status-widget/default.nix
+++ checks/wispr-status-widget/default.nix
@@ -13,11 +13,13 @@ pkgs.runCommand "wispr-status-widget" { nativeBuildInputs = [ noctalia ]; } ''
   cp "$plugin/WisprStatusState.luau" "$workdir/WisprStatusState.luau"
   cp "$plugin/WisprStatusService.luau" "$workdir/WisprStatusService.luau"
   cp "$plugin/BarWidget.luau" "$workdir/BarWidget.luau"
+  cp ${./state_behavior_test.luau} "$workdir/state_behavior_test.luau"
   cp ${./service_behavior_test.luau} "$workdir/service_behavior_test.luau"
   cp ${./widget_behavior_test.luau} "$workdir/widget_behavior_test.luau"
 
   cd "$workdir"
   noctalia plugins lint "$plugin"
+  ${pkgs.lua5_4}/bin/lua state_behavior_test.luau
   ${pkgs.lua5_4}/bin/lua service_behavior_test.luau
   ${pkgs.lua5_4}/bin/lua widget_behavior_test.luau
 
